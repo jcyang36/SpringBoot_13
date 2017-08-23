9b5f0a1466470b999ff17d31e47957c5c2669936
jcyang36 <jcyang@hmc.edu>
2017-08-22 20:33:58 -0400

    Final  commit

--- src/main/java/com/example/demo/Actor.java
+++ src/main/java/com/example/demo/Actor.java
@@ -13,4 +13,36 @@ public class Actor {
 
     @ManyToMany(mappedBy="cast")
     private Set<Movie> movies;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRealname() {
+        return realname;
+    }
+
+    public void setRealname(String realname) {
+        this.realname = realname;
+    }
+
+    public Set<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(Set<Movie> movies) {
+        this.movies = movies;
+    }
 }
